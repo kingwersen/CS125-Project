489b9adcf34b2db59768bcfdbcb1e690dc9b2cd9
Evelyn Cordero <eacaballero6710@gmail.com>
2018-03-19 12:58:12 -0700

    Added speech implementation(needs to be tested)

--- app/src/main/java/com/ingwersen/kyle/cs125_project/MainActivity.java
+++ app/src/main/java/com/ingwersen/kyle/cs125_project/MainActivity.java
@@ -35,6 +35,19 @@ import com.ingwersen.kyle.cs125_project.location.LocationManager;
 import java.util.ArrayList;
 import java.util.List;
 
+//import
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+//end
+
 public class MainActivity extends AppCompatActivity implements
         SuggestFragment.OnSuggestFragmentInteractionListener,
         CartFragment.OnCartFragmentInteractionListener,
@@ -95,6 +108,66 @@ public class MainActivity extends AppCompatActivity implements
 
         // TODO:
         // 2. Voice Input: https://developer.android.com/training/wearables/apps/voice.html
+    private TextView voiceInput;
+    private TextView speakButton;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_recognition);
+
+        voiceInput = (TextView) findViewById(R.id.voiceInput);
+        speakButton = (TextView) findViewById(R.id.btnSpeak);
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                askSpeechInput();
+            }
+        });
+
+    }
+
+    // Showing google speech input dialog
+
+    private void askSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Hi speak something");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    // Receiving speech input
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    voiceInput.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+    //end of my speech implementation 
+    
+    
         // 3. Image Input
     }
 
